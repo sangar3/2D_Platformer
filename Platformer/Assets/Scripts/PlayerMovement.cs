acd9ab765cbcea7c73460b776cca2a10cf422c73
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-02-26 23:01:04 -0600

    first commit

---
+++ Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D myrb;
+    public float movespeed;
+    public Animator myanim;
+    private bool facingRight;
+    void Start()
+    {
+        facingRight = true;
+
+    }
+
+    
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        HandleMovement(horizontal);
+        flip(horizontal);
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+        myrb.velocity = new Vector2(horizontal *movespeed , myrb.velocity.y);
+    }
+
+    private void flip(float horizontal)
+    {
+        if(horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale; // changing player X-axis Scale
+
+            theScale.x *= -1;
+            transform.localScale = theScale;
+
+
+        }
+    }
+}
