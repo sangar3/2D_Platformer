46a7ad265c221837047ad8785d7eff3c4d99123e
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-02-28 09:47:19 -0600

    commit

---
+++ Box Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;//use this when u want to change/restart scene
+public class GameManager : MonoBehaviour {
+    bool gamehasEnded = false;
+    public float restartDelay = 1f;
+
+    public GameObject completeLevel1UI;
+
+
+    public void EndGame()
+    {
+        if (gamehasEnded == false)
+        gamehasEnded = true;
+        Invoke("Restart", restartDelay);
+        Debug.Log("Game Over");
+    }
+
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+    public void CompleteLevel()
+    {
+        completeLevel1UI.SetActive(true);
+
+    }
+}
