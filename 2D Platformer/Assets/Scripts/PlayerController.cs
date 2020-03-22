59e21ae9592ebde260394816058b927ca1117888
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-03-22 02:01:31 -0500

    commit

---
+++ 2D Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movespeed;
+    public Rigidbody2D theRB;
+    public float jumpForce;
+    void Update()
+    {
+        theRB.velocity = new Vector2(movespeed * Input.GetAxis("Horizontal"), theRB.velocity.y); // left/right movement 
+
+        if(Input.GetButtonDown("Jump")) //jumping 
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+
+        }
+
+
+
+    }
+}
