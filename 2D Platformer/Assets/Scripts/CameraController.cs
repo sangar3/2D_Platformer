01c5269b77b4427c439416d6f78e85af8d1cc300
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-03-23 17:17:37 -0500

    paralax

---
+++ 2D Platformer/Assets/Scripts/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+
+    public Transform farbackground, middleBackground;
+    public float minHeight;
+    public float maxHeight;
+
+    private float lastXPos;
+
+
+    void Start()
+    {
+        lastXPos = transform.position.x;
+
+    }
+
+   
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+
+
+        float ClampedY = Mathf.Clamp(transform.position.y, minHeight, maxHeight); 
+        transform.position = new Vector3(transform.position.x, ClampedY, transform.position.z);
+        
+        float amountToMoveX = transform.position.x - lastXPos;
+
+
+        farbackground.position = farbackground.position + new Vector3(amountToMoveX, 0f, 0f);
+        middleBackground.position += new Vector3(amountToMoveX * .5f, 0f, 0f);
+
+        lastXPos = transform.position.x;
+
+
+    }
+}
--- 2D Platformer/Assets/Scripts/PlayerController.cs
+++ 2D Platformer/Assets/Scripts/PlayerController.cs
@@ -8,11 +8,21 @@ public class PlayerController : MonoBehaviour
     public float movespeed;
     public float jumpForce;
     private bool isGrounded;
+    private bool canDoubleJump;
 
     [Header("Fields")]
     public Rigidbody2D theRB;
     public Transform groundCheckPoint;
     public LayerMask whatIsGround;
+    private Animator anim;
+    private SpriteRenderer theSR;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        theSR = GetComponent<SpriteRenderer>();
+    }
 
     void Update()
     {
@@ -20,18 +30,41 @@ public class PlayerController : MonoBehaviour
 
         isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
 
+        if(isGrounded)
+        {
+            canDoubleJump = true;
 
+        }
         if(Input.GetButtonDown("Jump")) //jumping 
         {
             if(isGrounded)
             {
                 theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
             }
-            
+            else
+            {
+                if(canDoubleJump)
+                {
+                    theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+                    canDoubleJump = false;
+
+                }
+            }
 
         }
 
+        if(theRB.velocity.x < 0) //moving left 
+        {
+            theSR.flipX = true;
 
+        }
+        else if(theRB.velocity.x > 0) // moving right 
+        {
+            theSR.flipX = false;
 
+        }
+
+        anim.SetFloat("moveSpeed", Mathf.Abs(theRB.velocity.x));
+        anim.SetBool("isGrounded", isGrounded);
     }
 }
