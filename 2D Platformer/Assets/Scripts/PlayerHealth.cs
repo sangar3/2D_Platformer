8eff508258c786964fe837aff9dd611ccf293ece
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-03-26 11:29:49 -0500

    commit

--- 2D Platformer/Assets/Scripts/PlayerHealth.cs
+++ 2D Platformer/Assets/Scripts/PlayerHealth.cs
@@ -6,10 +6,17 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public static PlayerHealth instance;
-
+    [Header("InvincibilityNumbers")]
+    public float invincibleLength;
+    private float invincibleCounter;
+    public SpriteRenderer theSR;
     [Header("HealthNumbers")]
     public int currenthealth;
     public int maxHealth;
+   
+    
+    
+    
     void Start()
     {
         currenthealth = maxHealth;
@@ -24,21 +31,35 @@ public class PlayerHealth : MonoBehaviour
     
     void Update()
     {
-        
+        if(invincibleCounter > 0)
+        {
+            invincibleCounter -= Time.deltaTime;
+
+
+        }
     }
 
 
     public void DealDamage()
     {
-        currenthealth--;
-
-        if(currenthealth <=0)
+        if(invincibleCounter <=0)
         {
-            currenthealth = 0;
+            currenthealth--;
 
-            Debug.Log("Youre dead");
-            gameObject.SetActive(false);
+            if (currenthealth <= 0)
+            {
+                currenthealth = 0;
+
+                Debug.Log("Youre dead");
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                invincibleCounter = invincibleLength;
+                theSR.color = new Color(theSR.color.r, theSR.color.g, )
+            }
+            PlayerUIController.instance.UpdateHealthDisplay();
         }
-
+        
     }
 }
